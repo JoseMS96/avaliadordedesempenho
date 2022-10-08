87438abdfeef8fe3c1b13a6e5e035cfca362601d
lucasanderi <luc.anderi22@gmail.com>
2022-10-08 01:12:24 -0300

    refactor: correçao dos redirecionamentos das paginas

--- src/ADD - 0.02/ADD/add-client/src/main/java/br/fai/add/client/controller/FormController.java
+++ src/ADD - 0.02/ADD/add-client/src/main/java/br/fai/add/client/controller/FormController.java
@@ -24,52 +24,68 @@ public class FormController {
     public String getAnswerFormPage() {
         return "form/answer-form";
     }
-
-
-    @GetMapping("/list")
-    public String getForms(final Model model) {
-
-        List<Form> forms = formService.find();
-
-        if (forms == null || forms.isEmpty()) {
-            model.addAttribute("forms", new ArrayList<Form>());
-        } else {
-
-            model.addAttribute("forms", forms);
-        }
-
-        return "form/question-form"; //pagina para adicao de perguntas com os forms listados
+    @GetMapping("/question-form")
+    public String getQuestionPage(){return "form/question-form";}
+    @GetMapping("/employee-form")
+    public String getPageCollaborator(){
+        return "form/employee-form";
     }
-
-    @GetMapping("/detail/{id}")
-    public String getFormDetailPage(@PathVariable("id") final int id, final Model model) {
-
-        Form form = (Form) formService.findById(id);
-
-        if (form == null) {
-            return "form/view-form"; //pagina de vizualizar formulário com respostas dentro de perguntas
-        }
-
-        model.addAttribute("form", form);
-
-        return "form/view-form"; //pagina de vizualizar formulário com respostas dentro de perguntas
-    }
-
     @GetMapping("/create-form")
-    public String create(Form form) {
-
-        formService.create(form);
-        return "create-form"; //pagina para criar formulários até voltar?
+    public String getPageCreate(){
+        return "form/create-form";
+    }
+    @GetMapping("/details-form")
+    public String getPageDetailsForm(){
+        return "form/details-form";
     }
 
-    @GetMapping("/delete/{id}")
-    public String delete(@PathVariable("id") final int id, Model model) {
-
-        formService.deleteById(id);
 
 
-        return getForms(model);
-    }
+
+//    @GetMapping("/list")
+//    public String getForms(final Model model) {
+//
+//        List<Form> forms = formService.find();
+//
+//        if (forms == null || forms.isEmpty()) {
+//            model.addAttribute("forms", new ArrayList<Form>());
+//        } else {
+//
+//            model.addAttribute("forms", forms);
+//        }
+//
+//        return "form/details-form"; //pagina para adicao de perguntas com os forms listados
+//    }
+//
+//    @GetMapping("/detail/{id}")
+//    public String getFormDetailPage(@PathVariable("id") final int id, final Model model) {
+//
+//        Form form = (Form) formService.findById(id);
+//
+//        if (form == null) {
+//            return "form/view-form"; //pagina de vizualizar formulário com respostas dentro de perguntas
+//        }
+//
+//        model.addAttribute("form", form);
+//
+//        return "form/view-form"; //pagina de vizualizar formulário com respostas dentro de perguntas
+//    }
+//
+//    @GetMapping("/create-form")
+//    public String create(Form form) {
+//
+//        formService.create(form);
+//        return "form/create-form"; //pagina para criar formulários até voltar?
+//    }
+//
+//    @GetMapping("/delete/{id}")
+//    public String delete(@PathVariable("id") final int id, Model model) {
+//
+//        formService.deleteById(id);
+//
+//
+//        return getForms(model);
+//    }
 
 //    @GetMapping("/pending-form")
 //    public String getPendingFormPage() {
